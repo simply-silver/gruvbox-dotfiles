81cc20e07ef436462be4d5c4c7eb8310e9b4e131
ArgentSileo <argentsileo@gmail.com>
2019-10-29 18:15:52 -0500

    First commit

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+# Terminate already running bars
+killall -q polybar
+
+# Wait until bars have been terminated
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar mainscreen &
+polybar secondscreen &
